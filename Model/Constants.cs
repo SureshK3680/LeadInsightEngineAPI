43e622a853e8006ebaa2f46963e1093415ed2ed0
SureshK <sureshkumar.kamaraj@integrate.com>
2024-02-18 20:47:27 +0530

    Removed the confidential data from the repo

--- Model/Constants.cs
+++ Model/Constants.cs
@@ -5,11 +5,7 @@
         public const string EntitiesCollection = "entities";
         public const string EntityBatchCollection = "entitybatch";
         public const string AllocationCollection = "allocation";
-        //public const string ProdConnetionstring = "mongodb+srv://aws_mongo_production_user:lw$t4yWkr68qnotQ@prod-mongo.pi5jp.mongodb.net/neo_production?retryWrites=true&w=majority";
-        //public const string ProdAMQPCONNECTIONSTRING = "amqp://khwxtbjx:vHgWNJ6HixEj_AlczifWv8OQABcFQkHE@hefty-scorpion.rmq.cloudamqp.com/khwxtbjx";
-        //public const string StageConnetionstring = "mongodb+srv://aws_mongo_staging_user:9VffC6y7sgAcsnTK@staging-mongo.tiftg.mongodb.net/neo_staging?retryWrites=true&w=majority";
-        //public const string StageAMQPCONNECTIONSTRING = "amqp://lxkcpadp:Vlu3CkJjID99UVCzmCYA_oOETKooLoea@blossom-salamander.rmq.cloudamqp.com/lxkcpadp";
-        public const string Connetionstring = "mongodb+srv://aws_mongo_staging_user:9VffC6y7sgAcsnTK@staging-mongo.tiftg.mongodb.net/neo_staging?retryWrites=true&w=majority";
-        public const string AMQPCONNECTIONSTRING = "amqp://lxkcpadp:Vlu3CkJjID99UVCzmCYA_oOETKooLoea@blossom-salamander.rmq.cloudamqp.com/lxkcpadp";
+        public const string Connetionstring = "";
+        public const string AMQPCONNECTIONSTRING = "";
     }
 }
